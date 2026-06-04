9731b11d7ee44c7d066e7a442ac3566386ad8c73
danka0142 <danka0142@gmail.com>
2026-06-04 19:43:14 +0300

    rebuild for async
    заменил движок на асинхронный

--- app/database.py
+++ app/database.py
@@ -1,13 +1,16 @@
-from sqlalchemy import URL
-from sqlalchemy import create_engine
-#import pyodbc
+import asyncio                                   
+from sqlalchemy import URL, text                   
+from sqlalchemy.ext.asyncio import create_async_engine
 from dotenv import load_dotenv
 from os import getenv
+
 load_dotenv()
-API_KEY =getenv("API_KEY")
+API_KEY = getenv("API_KEY")
+
+#обьявляем подключение к базе и создаём асинхронный движок
 
 url_object = URL.create(
-    drivername = "postgresql+psycopg2",
+    drivername = "postgresql+asyncpg",
     username = getenv("postgre_login"),
     password = getenv("postgre_pwd"),
     host = getenv("postgre_ip"),
@@ -15,5 +18,17 @@ url_object = URL.create(
     database = "WeatherDB"
 )
 
-engine = create_engine(url_object)
+async_engine = create_async_engine(url_object)
 
+async def check_connection():
+    try:
+        async with async_engine.connect() as conn:
+            result = await conn.execute(text("SELECT 1"))
+            print(f"Успешное подключение! Результат: {result.scalar()}")
+    except Exception as e:
+        print(f"Ошибка подключения: {e}")
+    finally:
+        await async_engine.dispose()
+
+if __name__ == "__main__":
+    asyncio.run(check_connection())
